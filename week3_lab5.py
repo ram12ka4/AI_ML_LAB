b42e47e16acb041f5f73afc9b9a6bcbce912464d
Ram Kumar Basak <ram.kumar.basak@gmail.com>
2026-03-28 17:06:12 +0530

    Some modification

--- week3_lab5.py
+++ week3_lab5.py
@@ -48,30 +48,30 @@ b_tmp = -3
 print(compute_cost_logistic(X_train, y_train, w_tmp, b_tmp))
 
 # Choose values between 0 and 6
-x0 = np.arange(0,6)
+x0 = np.arange(0, 6)
 
 # Plot the two decision boundaries
 x1 = 3 - x0
 x1_other = 4 - x0
 
-fig,ax = plt.subplots(1, 1, figsize=(4,4))
+fig, ax = plt.subplots(1, 1, figsize=(4, 4))
 # Plot the decision boundary
-ax.plot(x0,x1, c=dlc["dlblue"], label="$b$=-3")
-ax.plot(x0,x1_other, c=dlc["dlmagenta"], label="$b$=-4")
-ax.axis([0, 4, 0, 4])
+ax.plot(x0, x1, c=dlc["dlblue"], label="$b$=-3")
+ax.plot(x0, x1_other, c=dlc["dlmagenta"], label="$b$=-4")
+ax.axis([0, 4, 0, 4]) # type: ignore
 
 # Plot the original data
-plot_data(X_train,y_train,ax)
+plot_data(X_train, y_train, ax)
 ax.axis([0, 4, 0, 4])
-ax.set_ylabel('$x_1$', fontsize=12)
-ax.set_xlabel('$x_0$', fontsize=12)
+ax.set_ylabel("$x_1$", fontsize=12)
+ax.set_xlabel("$x_0$", fontsize=12)
 plt.legend(loc="upper right")
 plt.title("Decision Boundary")
 plt.show()
 
-w_array1 = np.array([1,1])
+w_array1 = np.array([1, 1])
 b_1 = -3
-w_array2 = np.array([1,1])
+w_array2 = np.array([1, 1])
 b_2 = -4
 
 print("Cost for b = -3 : ", compute_cost_logistic(X_train, y_train, w_array1, b_1))
